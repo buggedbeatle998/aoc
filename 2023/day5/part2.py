09efe7a96a104b7706daa5044abdc391cb44318a
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2023-12-06 20:27:25 +0000

    help

--- 2023/day5/part2.py
+++ 2023/day5/part2.py
@@ -1,27 +1,36 @@
-"""seeds=[([int(open("data.txt","r").read().splitlines()[0].split(" ")[1::2][i]),int(open("data.txt","r").read().splitlines()[0].split(" ")[1::2][i])+int(open("data.txt","r").read().splitlines()[0].split(" ")[2::2][i])]) for i in range(int(len(open("data.txt","r").read().splitlines()[0].split(" ")[1:])/2))]
-for section in open("data.txt","r").read().split(":")[2:]:
-    for dest,source,ran in [(int(k) for k in n.split(" ")) for n in [i for i in (section.split("\n")[:-2] if (section.splitlines()[-1][-1].isalpha()) else section.split("\n"))][1:]]:
-        for i in range(len(seeds)):
-            if source > seeds[i][0] and source < seeds[i][1]:
-                seeds.append([source+(dest-source if (source+ran>=seeds[i][1]) else 0),seeds[i][0]+seeds[i][1]+(dest-source if (source+ran>=seeds[i][1]) else 0)])
-                seeds[i][1]=source
-            elif source+ran<seeds[i][1] and source+ran>seeds[i][0]:
-                seeds.append([seeds[i][0]+(dest-source if (source<=seeds[i][0]) else 0),source+ran+(dest-source if (source<=seeds[i][0]) else 0)])
-                seeds[i][0]=source+ran
-            elif source<=seeds[i][0] and source+ran>=seeds[i][1]: seeds[i]=[seeds[i][0]-source+dest,seeds[i][1]-source+dest]
-    print(sorted(seeds))"""
-
-answer = 46
 seeds=[([int(open("data.txt","r").read().splitlines()[0].split(" ")[1::2][i]),int(open("data.txt","r").read().splitlines()[0].split(" ")[1::2][i])+int(open("data.txt","r").read().splitlines()[0].split(" ")[2::2][i])]) for i in range(int(len(open("data.txt","r").read().splitlines()[0].split(" ")[1:])/2))]
+for section in open("data.txt","r").read().split(":")[2:]:
+    i=0
+    other=[]
+    while True:
+        if i>=len(seeds): seeds+=other;break
+        for dest,source,ran in [(int(k) for k in n.split(" ")) for n in (section.split("\n")[:-2] if (section.splitlines()[-1][-1].isalpha()) else section.split("\n"))][1:]:
+            if source > seeds[i][0] and source < seeds[i][1]:
+                other.append([source+(dest-source if (source+ran>=seeds[i][1]) else 0),seeds[i][1]+(dest-source if (source+ran>=seeds[i][1]) else 0)])
+                seeds[i][1]=source
+                if source+ran<other[-1][1] and source+ran>other[-1][0]:
+                    seeds.append([source+ran,other[-1][1]])
+                    other[-1]=[dest,dest+ran]
+                break
+            elif source+ran<seeds[i][1] and source+ran>seeds[i][0]:
+                other.append([seeds[i][0]+(dest-source),dest+ran])
+                seeds[i][0]=source+ran
+                break
+            elif source<=seeds[i][0] and source+ran>=seeds[i][1]:
+                seeds[i]=[seeds[i][0]-source+dest,seeds[i][1]-source+dest];break
+        i+=1
+print(min(seeds))
+"""answer = 12403039
+seeds=sorted([([int(open("data.txt","r").read().splitlines()[0].split(" ")[1::2][i]),int(open("data.txt","r").read().splitlines()[0].split(" ")[1::2][i])+int(open("data.txt","r").read().splitlines()[0].split(" ")[2::2][i])]) for i in range(int(len(open("data.txt","r").read().splitlines()[0].split(" ")[1:])/2))])
 while True:
     answered=answer
-    for section in open("data.txt","r").read().split(":")[:2:-1]:
-        answer=answered
-        for dest,source,ran in [(int(k) for k in n.split(" ")) for n in [i for i in (section.split("\n")[1:-2] if (section.splitlines()[-1][-1].isalpha()) else section.split("\n"))][1:]]:
-            if answer>=dest and answer<dest+ran:
-                answered=answer+source-dest
+    for section in open("data.txt","r").read().split(":")[:1:-1]:
+        for n in section.splitlines()[(section.splitlines()[0]==""):(len(section.splitlines())-2*(not section.splitlines()[0]==""))]:
+            dest,source,ran=n.split()
+            if answered>=int(dest) and answered<int(dest)+int(ran):answered+=int(source)-int(dest);break
     for seed in seeds:
         if answered>=seed[0] and answered<seed[1]:
-            break
+            answered=-5
+    if answered==-5: break
     answer+=1
-print(answered)
+print(answer)"""

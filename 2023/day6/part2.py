0fc6a16a63cff73250dcee27a4c6b10b0bd0f14c
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2023-12-07 11:32:46 +0000

    day 6

--- 2023/day6/part2.py
+++ 2023/day6/part2.py
@@ -1,5 +1,5 @@
 import math
-tot,data=1,[" ".join(open("data.txt","r").read().split()).split(":")[1].split()[:-1]," ".join(open("data.txt","r").read().split()).split(":")[2].split()]
+tot,data=1,["".join(open("data.txt","r").read().split(" ")).split(":")[1].split()[:-1],"".join(open("data.txt","r").read().split(" ")).split(":")[2].split()]
 for i in range(len(data[0])):
     tot*=math.floor(((-int(data[0][i])-math.sqrt(int(data[0][i])**2-(4*int(data[1][i]))))/-2)-0.000000000001)-math.floor((-int(data[0][i])+math.sqrt(int(data[0][i])**2-(4*int(data[1][i]))))/-2)
 print(tot)

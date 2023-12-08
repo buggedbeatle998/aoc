4aa1e636f8abfa536a711e789da84f10ffecd9e7
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2023-12-08 18:03:43 +0000

    day8 finished

--- 2023/day8/part2.py
+++ 2023/day8/part2.py
@@ -1,9 +1,10 @@
-import re
-dirs,mapped,place,tot=open("data.txt","r").read().splitlines()[0],{},re.findall(r"(..A) =",open("data.txt","r").read()),0
+import re,math
+dirs,mapped,place,tot,mults=open("data.txt","r").read().splitlines()[0],{},re.findall(r"(..A) =",open("data.txt","r").read()),0,[]
 for i in open("data.txt","r").read().splitlines()[2:]: mapped.update({re.findall(r"\w{3}",i)[0]:re.findall(r"\w{3}",i)[1:]})
-while True:
-    for i in range(len(place)):
-        place[i]=mapped[place[i]][dirs[tot%len(dirs)]=="R"]
+while len(place)>0:
+    for i in range(len(place)): place[i]=mapped[place[i]][dirs[tot%len(dirs)]=="R"]
     tot+=1
-    if all([i[-1]=="Z" for i in place]): print(place)
-print(tot)
+    for i in range(len(place)):
+        if i>=len(place): break
+        if place[i][-1]=="Z": mults.append(tot);place.pop(i)
+print(math.lcm(*mults))

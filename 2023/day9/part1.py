ba71185430a6467bd6ca6f97574e27e6afd48dbb
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2023-12-09 15:10:16 +0000

    day 9

--- 2023/day9/part1.py
+++ 2023/day9/part1.py
@@ -1,4 +1,9 @@
-import numpy
-for seqence in [[eval(n) for n in i.split()] for i in open("data.txt","r").read().splitlines()]:
-    print(seqence)
-    print(numpy.diff(seqence))
+import numpy,more_itertools
+tot=0
+for sequence in [[[eval(n) for n in i.split()]] for i in open("data.txt","r").read().splitlines()]:
+    while not more_itertools.all_equal(sequence[-1]):
+        sequence.append(list(numpy.diff(sequence[-1])))
+    for i in range(len(sequence)-1):
+        sequence[-(i+2)].insert(0,sequence[-(i+2)][0]-sequence[-(i+1)][0])
+    tot+=sequence[0][0]
+print(tot)

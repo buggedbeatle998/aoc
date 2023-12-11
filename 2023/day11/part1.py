ddc0e098b17626a500799530f41da2537028a7d4
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2023-12-11 22:11:37 +0000

    11.5

--- 2023/day11/part1.py
+++ 2023/day11/part1.py
@@ -1,14 +1,10 @@
-data,tot=[[*i] for i in open("data.txt","r").read().splitlines()],0
+data,tot,width=[[[n,1] for n in i] for i in open("data.txt","r").read().splitlines()],0,1000000
 for i in range(len(data[0]))[::-1]:
-    if all([False if data[n][i]=="#" else True for n in range(len(data))]):
-        for n in range(len(data)): data[n].insert(i,".")
+    if all([False if data[n][i][0]=="#" else True for n in range(len(data))]):
+        for n in range(len(data)): data[n][i][1]=width
 for i in range(len(data))[::-1]:
-    if not "#" in data[i]: data.insert(i,data[i])
-for source in [[(x,y) for x,i in enumerate(line) if i=="#"][0] for y,line in enumerate(data) if "#" in line]:
-    data[source[1]][source[0]]="@"
-    for i in data:
-        print(i)
-    print()
-    for dest in [[(x,y) for x,i in enumerate(line) if i=="#"][0] for y,line in enumerate(data) if "#" in line]: tot+=(abs(source[0]-dest[0])+abs(source[1]-dest[1])) if source!=[] and dest!=[] else 0
-    data[source[1]][source[0]]="."
+    if not ["#",1] in data[i] and not ["#",width] in data[i]: data[i]=[[".",width]]*len(data[i])
+for spos,source in [item for sublist in [[((x,y),(sum([data[y][xx][1] for xx in range(x)]),sum([data[yy][x][1] for yy in range(y)]))) for x,i in enumerate(line) if i[0]=="#"] for y,line in enumerate(data) if ["#",1] in line or ["#",width] in line] for item in sublist]:
+    data[spos[1]][spos[0]][0]="."
+    for dpos,dest in [item for sublist in [[((x,y),(sum([data[y][xx][1] for xx in range(x)]),sum([data[yy][x][1] for yy in range(y)]))) for x,i in enumerate(line) if i[0]=="#"] for y,line in enumerate(data) if ["#",1] in line or ["#",width] in line] for item in sublist]: tot+=(abs(source[0]-dest[0])+abs(source[1]-dest[1])) if source!=[] and dest!=[] else 0
 print(tot)

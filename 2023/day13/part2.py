5b475dc990af2378d5e81b81633932321ae0845c
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2023-12-13 13:52:18 +0000

    13.0

---
+++ 2023/day13/part2.py
@@ -0,0 +1,9 @@
+tot=0
+for section in []:#[i.splitlines() for i in open("data.txt","r").read().split("\n\n")]:
+    for y in range(1,len(section)):
+        if sum(section[max(0,y-(len(section)-y)):y])==section[y:min(len(section),y*2):][::-1]: tot+=y*100;break
+    else:
+        for x in range(1,len(section[0])):
+            if [i[max(0,x-(len(section[0])-x)):x] for i in section]==[i[x:min(len(section[0]),x*2):][::-1] for i in section]: tot+=x;break
+print(tot)
+print(set([1,2,3,4,5])-set([3,4,5,6,7]))

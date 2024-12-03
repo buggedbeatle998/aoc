a4f21290b757c72993701d43d48ff7547c622d64
beatle <118904398+buggedbeatle998@users.noreply.github.com>
2024-12-03 08:51:23 +0000

    day3 part2

--- 2024/day3/part2.cpp
+++ 2024/day3/part2.cpp
@@ -1,90 +1,31 @@
-#include<algorithm>
 #include<fstream>
 #include<iostream>
-#include<string>
-#include<sstream>
-#include<cmath>
+#include<regex>
 
 using namespace std;
 
 
 int main() {
     ifstream data("input.txt");
+    std::string input((std::istreambuf_iterator<char>(data)), std::istreambuf_iterator<char>());
     int total = 0;
 
-    string line;
-    while (getline(data, line)) {
-        string num;
-        bool safe = false;
-        int ignore = std::count(line.begin(), line.end(), ' ') + 2;
-        while (!safe && ignore-- > 0) {
-            //cout << '\n';
-            int i = 0;
-            int prev = -1;
-            short int isdec = -1;
-            stringstream ss(line);
-            while (getline(ss, num, ' ')) {
-                if (i++ == ignore) {
-                    continue;
-                }
-                //cout << num << ' ';
-                if (prev != -1 && (abs(prev - stoi(num)) > 3 || prev - stoi(num) == 0 || (isdec != -1 && signbit(stoi(num) - prev) != isdec))) {
-                    safe = false;
-                    break;
-                }
-                if (prev != -1) {
-                    isdec = signbit(stoi(num) - prev);
-                }
-                prev = stoi(num);
-                safe = true;
-            }
+    regex mulregex("don't\\(\\)|do\\(\\)|mul\\(([0-9]+),([0-9]+)\\)");
+    smatch matched;
+    string::const_iterator start(input.cbegin());
+    bool can = true;
+    while (regex_search(start, input.cend(), matched, mulregex)) {
+        cout << matched[0];
+        if (matched[0] == "do()") can = true;
+        else if (matched[0] == "don't()") can = false;
+        else if (can) {
+            cout << matched[1] << ' ' << matched[2] << '\n';
+            total += stoi(matched[1]) * stoi(matched[2]);
         }
-        if (safe) {
-            total++;
-        }
+        start = matched.suffix().first;
     }
 
     cout << total << '\n';
 
     return 0;
 }
-
-
-// int main() {
-//     ifstream data("test.txt");
-//     int total = 0;
-
-//     string line;
-//     while (getline(data, line)) {
-//         int prev = -1;
-//         string num;
-//         total++;
-//         stringstream ss(line);
-//         short int valid = 2;
-//         vector<int> curr;
-//         while (getline(ss, num, ' ')) {
-//             curr.push_back(stoi(num));
-//         }
-//         int lsize = curr.size();
-//         if (curr[0] == curr[1]) {
-//             break;
-//         }
-//         for (int i = 1; i < lsize - 1; i++) {
-//             if (abs(curr[i] - curr[i - 1]) > 3 && abs(curr[i + 1] - curr[i - 1]) > 3) {
-//                 valid--;
-//             }
-//             if (signbit(curr[i] - curr[i - 1]) ^ signbit(curr[i + 1] - curr[i])) {
-//                 bool left = signbit(curr[i] - curr[i - 1]);
-//                 bool right = signbit(curr[i + 1] - curr[i]);
-//                 if (i != 1) {
-                    
-//                 }
-//             }
-//         }
-
-//     }
-
-//     cout << total << '\n';
-
-//     return 0;
-// }

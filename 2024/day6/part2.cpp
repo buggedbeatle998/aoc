dba204ddc05f008663a007f06ae62f3c807cac5a
buggedbeatle998 <118904398+buggedbeatle998@users.noreply.github.com>
2024-12-06 18:57:24 +0000

    day 6 part 2

--- 2024/day6/part2.cpp
+++ 2024/day6/part2.cpp
@@ -1,8 +1,10 @@
 #include<fstream>
 #include<iostream>
 #include<string>
+#include <utility>
 #include<vector>
-#include<algorithm>
+#include<unordered_map>
+#include<cmath>
 
 using namespace std;
 
@@ -26,11 +28,10 @@ int main() {
         }
     }
 
-    while (0 <= (pos[0] += dirs[dir].first) && pos[0] < board[0].size() && 0 <= (pos[1] += dirs[dir].second) && pos[1] < board.size()) {
-        //cout << pos[0] << ' ' << pos[1] << '\n';
+    while (0 <= pos[0] && pos[0] < board[0].size() && 0 <= pos[1] && pos[1] < board.size()) {
         char posh = board[pos[1]][pos[0]];
         char posr = board[pos[1] + dirs[(dir + 1) % 4].second][pos[0] + dirs[(dir + 1) % 4].first];
-        char posf;
+        char posf = 'X';
         if (0 <= pos[1] + dirs[dir].second && pos[1] + dirs[dir].second < board.size() && 0 <= pos[0] + dirs[dir].first && pos[0] + dirs[dir].first < board[0].size())
             posf = board[pos[1] + dirs[dir].second][pos[0] + dirs[dir].first];
 
@@ -38,36 +39,43 @@ int main() {
             pos[0] -= dirs[dir].first;
             pos[1] -= dirs[dir].second;
             dir = (dir + 1) % 4;
+            posh = board[pos[1]][pos[0]];
+            posr = board[pos[1] + dirs[(dir + 1) % 4].second][pos[0] + dirs[(dir + 1) % 4].first];
+            posf = '#';
+            if (0 <= pos[1] + dirs[dir].second && pos[1] + dirs[dir].second < board.size() && 0 <= pos[0] + dirs[dir].first && pos[0] + dirs[dir].first < board[0].size())
+                posf = board[pos[1] + dirs[dir].second][pos[0] + dirs[dir].first];
         } else if (board[pos[1]][pos[0]] == '.')
             board[pos[1]][pos[0]] = '^';
-        if (posf != '^' && posf != '@' && posf != '#' && posr != '#') {
-            dir = (dir + 1) % 4;
+        if (posf != 'X' && posf != '^' && posf != '@' && posf != '#') {
+            short ddir = (dir + 1) % 4;
+            board[pos[1] + dirs[dir].second][pos[0] + dirs[dir].first] = '#';
             dpos[0] = pos[0];
             dpos[1] = pos[1];
-            while (0 <= (dpos[0] += dirs[dir].first) && dpos[0] < board[0].size() && 0 <= (dpos[1] += dirs[dir].second) && dpos[1] < board.size()) {
-                //cout << '\t' << dpos[0] << ' ' << dpos[1] << '\n';
+            unordered_map<int, short> obshit;
+            while (0 <= (dpos[0] += dirs[ddir].first) && dpos[0] < board[0].size() && 0 <= (dpos[1] += dirs[ddir].second) && dpos[1] < board.size()) {
                 if (board[dpos[1]][dpos[0]] == '#') {
-                    dpos[0] -= dirs[dir].first;
-                    dpos[1] -= dirs[dir].second;
-                    char dposh = board[dpos[1]][dpos[0]];
-                    char dposr = board[dpos[1] + dirs[(dir + 1) % 4].second][dpos[0] + dirs[(dir + 1) % 4].first];
-                    if ((dposr == '^' || dposr == '@') && (dposh == '^' || dposh == '@')) {
-                        if (0 <= pos[1] + dirs[(dir + 3) % 4].second && pos[1] + dirs[(dir + 3) % 4].second < board.size() && 0 <= pos[0] + dirs[(dir + 3) % 4].first && pos[0] + dirs[(dir + 3) % 4].first < board[0].size()) {
-                            board[pos[1] + dirs[(dir + 3) % 4].second][pos[0] + dirs[(dir + 3) % 4].first] = '@';
-                            //cout << "\t\t" << pos[0] + dirs[(dir + 3) % 4].first << ' ' << pos[1] + dirs[(dir + 3) % 4].second << '\n';
+                    if (obshit.count(dpos[0] * 1000 + dpos[1])) {
+                        if (obshit[dpos[0] * 1000 + dpos[1]] & (short)pow(2, ddir)) {
                             total++;
-                    }
-                    }
-                    break;
+                            break;
+                        } else
+                            obshit[dpos[0] * 1000 + dpos[1]] |= (short)pow(2, ddir);
+                    } else
+                        obshit[dpos[0] * 1000 + dpos[1]] = (short)pow(2, ddir);
+                    dpos[0] -= dirs[ddir].first;
+                    dpos[1] -= dirs[ddir].second;
+                    ddir = (ddir + 1) % 4;
                 }
             }
-            dir = (dir + 3) % 4;
+            board[pos[1] + dirs[dir].second][pos[0] + dirs[dir].first] = '@';
         }
+        pos[0] += dirs[dir].first;
+        pos[1] += dirs[dir].second;
     }
 
-    for (int i = 0; i < board.size(); i++) {
-        cout << board[i] << '\n';
-    }
+    //for (int i = 0; i < board.size(); i++) {
+        //cout << board[i] << '\n';
+    //}
     cout << total << '\n';
 
     return 0;
